9e459e74e1275a2d4606f96b7409c86fb6a0c7e0
Anurup sen <anurupsen2009@gmail.com>
2024-08-14 13:37:13 +0600

    script ubdate

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
-var message="Sorry, Selection is disabled. Message from Anurup sen";
+// var message="Sorry, Selection is disabled. Message from Anurup sen";
 
- function clickIE() {if (document.all) {alert(message);return false;}}
- function clickNS(e) {if 
- (document.layers||(document.getElementById&&!document.all)) {
- if (e.which==2||e.which==3) {alert(message);return false;}}}
- if (document.layers) 
- {document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
- else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
+// function clickIE() {if (document.all) {alert(message);return false;}}
+// function clickNS(e) {if 
+// (document.layers||(document.getElementById&&!document.all)) {
+// if (e.which==2||e.which==3) {alert(message);return false;}}}
+// if (document.layers) 
+// {document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
+// else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
 
- document.oncontextmenu=new Function("return false")
+// document.oncontextmenu=new Function("return false")
 
 
 
-function disableselect(e){
-return false
-}
-function reEnable(){
-return true
-}
-document.onselectstart=new Function ("return false")
-if (window.sidebar){
-document.onmousedown=disableselect
-document.onclick=reEnable
-}
+// function disableselect(e){
+// return false
+// }
+// function reEnable(){
+// return true
+// }
+// document.onselectstart=new Function ("return false")
+// if (window.sidebar){
+// document.onmousedown=disableselect
+// document.onclick=reEnable
+// }
 
 // cancel selection 
 
@@ -50,22 +50,3 @@ document.querySelector('#menu-btn').onclick = () =>{
 window.onscroll = () =>{
     navbar.classList.remove('active');
 }
-
-
-// video section start
-let mainVid = document.querySelector('.main-video');
-
-document.querySelectorAll('video').forEach(vid => {
-    vid.onclick = () => {
-        let src = vid.getAttribute('src');
-        mainVid.classList.add('active');
-        mainVid.querySelector('video').src = src;
-    }
-});
-
-document.querySelector('#close-vid').onclick = () => {
-    mainVid.classList.remove('active');
-    mainVid.querySelector('video').pause(); // Pause the video
-}
-
-
